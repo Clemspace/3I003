5085e0a0af721ffff7212f0770d1f20484a1d2a1
Clemspace <clement.castellon@gmail.com>
2018-12-03 15:34:04 +0100

    updated code

--- Fonctions.py
+++ Fonctions.py
@@ -1,12 +1,72 @@
-import math
+import numpy as np
+import time
 
+
+def lecturefichier(f):
+
+    file = open(f, "r")
+
+
+    S = file.readline()
+    k = file.readline()
+
+    V = [[0] * k for _ in range(S)]
+
+    V[0] = list(map(int, file.readline(k).split()))
+
+    file.close()
+
+    return V
+
+def genfichiertests(nom, k, pmax, s):
+    """
+    crée un fichier txt avec :
+       ligne 1: valeur s > pmax, plus petite que smax
+       ligne 2: valeur k
+       ligne 3: tableaux trié des capacités croissantes dont la dernière est inférieure a pmax.
+    """
+
+    file = open(nom, "w+")
+
+    #s = np.random.randint()
+    #k = np.random.randint(2, high = k)
+    v = 1 + np.random.randint(2, high = pmax, size = k-1)
+    v.sort()
+
+    file.write(s + "\n")
+    file.write(k + "\n")
+    file.write(v)
+
+    file.close()
+
+
+def timerfunc(func):
+    """
+    A timer decorator
+    """
+
+    def function_timer(*args, **kwargs):
+        """
+        A nested function for timing other functions
+        """
+        start = time.time()
+        value = func(*args, **kwargs)
+        end = time.time()
+        runtime = end - start
+        msg = "The runtime for {func} took {time} seconds to complete"
+        print(msg.format(func=func.__name__,
+                         time=runtime))
+        return value
+
+    return function_timer
 
 def rechercheexhaustive(k, v, s):
+
     i, nbcont, x = 1, 0, 0
 
     if s < 0:
 
-        return math.inf
+        return float("inf")
 
     else:
 
@@ -46,12 +106,48 @@ def m(s, i, v):
 
     elif i == 0 and s >= 1:
 
-        return math.inf
+        return float("inf")
 
     elif s < 0 and i > 1:
 
-        return math.inf
+        return float("inf")
 
     else:
 
         return min( m(s, i-1, v), m(s-v[i], i, v)+1)
+
+
+def algoglouton(S, v):
+
+    res = 0
+    i = range(v[0])
+
+    while S > 0:
+
+        if S < v[0][i] :
+
+            i-=1
+
+        elif S >= v[0][i]:
+
+            res += S // v[0][i]
+            i-=1
+            S = S - (S // v[0][i]) * v[0][i]
+
+    return res
+
+
+def TestGloutonCompatible(k, v):
+
+    j,S = 0, 0
+
+    if k >= 3:
+
+        for S in range(v[3]+2, v[k-1] + v[k]-1):
+
+            for j in range (1,k):
+
+                if v[j] < S & algoglouton(S) > 1+algoglouton(S-v[0][j]):
+
+                    return False
+    return True
--- exec.py
+++ exec.py
@@ -1,2 +1,9 @@
 
 
+def question12():
+    """
+    trace les courbes de temps d'execution en fonction de s et k
+    """
+
+
+def question14():
